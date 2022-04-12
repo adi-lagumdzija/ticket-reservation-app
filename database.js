4a5b23c25505cb1f3c10f2f8fc6897290f5f2114
adilagumdzija <adi.lagumdzija@stu.ibu.edu.ba>
2022-04-12 14:31:00 +0200

    Database successfully connected

--- database.js
+++ database.js
@@ -3,10 +3,17 @@ import mysql from 'mysql';
 var con = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'bradi123'
+    password: 'bradi123',
+    database : "ticket-reservation"
   });
   
+
+
   con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    con.query("SELECT username FROM users", function (err, result, fields) {
+      if (err) throw err;
+      // if there is no error, you have the result
+      console.log(result);
+    });
   });
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import usersRoutes from './routes/users.js';
 //nodemon imported into project - automatically restarting the node application when file changes in the directory are detected.
 const app = express();
 const PORT = 5050; //backend port
+// app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`); });
 const HOST = 'localhost';
 app.use(bodyParser.json()); //using json through our app
 app.use('/users', usersRoutes);
@@ -12,4 +13,4 @@ app.get('/', (req, res) => {
     res.send("Hello from homepage");
 });
 
-app.listen(PORT, () => console.log(`Server runing on port: http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: http://${HOST}:${PORT}`));
