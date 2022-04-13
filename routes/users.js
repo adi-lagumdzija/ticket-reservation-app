366f9c2ffb931bc5d9d2396082f02781f8c386f0
adilagumdzija <adi.lagumdzija@stu.ibu.edu.ba>
2022-04-13 07:41:27 +0200

    adding swagger documentation

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,76 @@
 import express from 'express';
+import mysql from 'mysql';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+var con = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'bradi123',
+    database : "ticket-reservation"
+  });
 
 const router = express.Router();
 
-//all routes starting with /users 
-router.get('/', (req, res) => {
-    res.send('Hello from route "/users"');
-});
 
-router.get('/user', (req, res) => {
-    res.send('Hello from route "/users/user"');
+const options ={
+    definition: {
+        openapi : '3.0.0',
+        info : {
+            title : 'Ticket Reservation Project',
+            version : '1.0.0'
+        },
+        servers : [
+            {
+            url : 'http://localhost:5050/'
+            }
+    ]
+    },
+    apis : ['routes/users.js']
+}
+const swaggerSpec = swaggerJsDoc(options)
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *      summary: This API is used to get all users API
+ *      description: This API is used to get all users API
+ *      responses:
+ *          '200':
+ *              description: testing get method
+ */
+router.get('/', (req, res) => {
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
+    });
+/**
+ * @swagger
+ * /users/usernames:
+ *  get:
+ *      summary: This Api is used to get usernames 
+ *      description: This Api is used to get usernames
+ *      responses:
+ *          '200':
+ *              description: testing get method
+ */
+router.get('/usernames', (req, res) => {
+    con.query("SELECT username FROM users", function (err, result, fields) {
+        if (err) throw err;
+        // if there is no error, you have the result
+        res.send(result);
+      });
 })
+
+
+// router.get('/:id', (req, res) => {
+//     con.query("SELECT * FROM users WHERE id = "+req.params.id;, function (err, result, fields) {
+//         if (err) throw err;
+//         // if there is no error, you have the result
+//         res.send(result);
+//       });
+// })
 
 export default router;
